6ad5996ccb3baa6427d7dc28afd2650a95dd0d51
Patricia Geronimo <ptrcgrnm1@gmail.com>
2024-05-11 10:46:19 +0800

    Add files via upload
    Percentage Calculator
    Percentage Coding
    This is only for Educational Purposes

---
+++ Tip_Percentage.py
@@ -0,0 +1,8 @@
+x=(input("Please enter the price of your meal : "))
+y=(input("Please enter the percent of tip you want to leave : "))
+
+t=x*(y/100)
+b=x+y
+
+print("The tip you gave is ", t)
+print("The total bill you have including the tip is ", b)
