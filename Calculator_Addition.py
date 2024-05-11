ad2d3c93c15e5e4711acfa5221084c4423549470
Patricia Geronimo <ptrcgrnm1@gmail.com>
2024-05-11 10:44:07 +0800

    Add files via upload
    Calculator Code  
    This can only do addition.
    This is only for Educational Purposes.

---
+++ Calculator_Addition.py
@@ -0,0 +1,6 @@
+a=eval(input("Type a number: "))
+b=eval(input("Type your second number: "))
+c=eval(input("Type your third number: "))
+
+print("The sum of the three numbers is ", a+b+c)
+print("The average of the three numbers is ", (a+b+c)/3)
